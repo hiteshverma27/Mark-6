a86bb1fab8189d241079e74db0cb04cdff636b08
hiteshverma27 <87027579+hiteshverma27@users.noreply.github.com>
2021-09-22 15:26:40 +0530

    added nav bar and footer

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector('#text-input');
 var output = document.querySelector('#output');
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 function getTranslationURL(text){
     return serverURL+"?"+"text="+text
 }
