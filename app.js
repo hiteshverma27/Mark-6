0e675af06da5830181d6f48965ef49d9d5c20ee6
hiteshverma27 <87027579+hiteshverma27@users.noreply.github.com>
2021-09-22 15:08:37 +0530

    added mock server URL

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("working")
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector('#text-input');
+var output = document.querySelector('#output');
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+function errorMsg(){
+    alert("Error occured while trying to reach server, please try again later.")
+}
+
+function clickHandle(){
+    var inputText = textInput.value
+    fetch(getTranslationURL(inputText))
+    .then(Response=>Response.json())
+    .then(json=>output.innerText=json.contents.translated)
+    .catch(errorMsg)
+}
+btnTranslate.addEventListener("click", clickHandle)
